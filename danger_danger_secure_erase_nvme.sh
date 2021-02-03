fd318b967d69cfdc8ada34fc56206cd3d7b02edc
James Wilkins <jiwilkins@redhat.com>
2021-02-03 13:44:10 +0000

    nvme erase script

--- danger_danger_secure_erase_nvme.sh
+++ danger_danger_secure_erase_nvme.sh
@@ -3,8 +3,12 @@
 # Script to perform a cryptographic secure erase on all NVME devices on the PPB test kit
 #
 # This is *not* launched by ansible due to its destructive nature.
+echo "This script *will* irrevocably erase the contents of /dev/nvme[0-23]"
+echo "If you do *NOT* wish this to happen - stop this script now"
+sleep 10
+read -n 1 -s -r -p "Press any key to continue"
 for i in {0..23}
 do
  echo "=> Working on /dev/nvme$i"
- nvme format /dev/nvme0 -ses 2 -n 1
+ nvme format /dev/nvme$i -ses 2 -n 1
 done
